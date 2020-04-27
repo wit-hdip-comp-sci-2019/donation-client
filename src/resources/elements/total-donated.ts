5fad8516ff2e67ad52c2d9c2fb2318a3e63bc5b6
Eamonn de Leastar <edeleastar@wit.ie>
2020-04-27 10:14:46 +0100

    subscribe to TotalUpdated message

--- src/resources/elements/total-donated.ts
+++ src/resources/elements/total-donated.ts
@@ -1,11 +1,17 @@
 import { inject } from 'aurelia-framework';
 import { DonationService } from '../../services/donation-service';
 import { bindable } from 'aurelia-framework';
+import { TotalUpdate } from '../../services/messages';
+import { EventAggregator } from 'aurelia-event-aggregator';
 
-@inject(DonationService)
+@inject(DonationService, EventAggregator)
 export class TotalDonated {
-  @bindable
   total = 0;
 
-  constructor(private ds: DonationService) {}
+  constructor(private ds: DonationService, private ea: EventAggregator) {
+    this.total = ds.total;
+    ea.subscribe(TotalUpdate, msg => {
+      this.total = msg.total;
+    });
+  }
 }
