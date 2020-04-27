d7ce8c6c3bcc25fbc422ed6befa33e75a36ece8b
Eamonn de Leastar <edeleastar@wit.ie>
2020-04-27 10:08:47 +0100

    place total-donated on donate view

--- src/views/donate.ts
+++ src/views/donate.ts
@@ -7,10 +7,12 @@ export class Donate {
   donations: Donation[];
   paymentMethods: string[];
   candidates: Candidate[];
+  total = 0;
 
   constructor(private ds: DonationService) {
     this.candidates = ds.candidates;
     this.donations = ds.donations;
     this.paymentMethods = ds.paymentMethods;
+    this.total = ds.total;
   }
 }
