fb3b081e710e33193d7d76c4749fec0d8e393a0a
Eamonn de Leastar <edeleastar@wit.ie>
2020-04-27 11:06:36 +0100

    wire up login - and change router once user is authenticated

--- src/services/donation-service.ts
+++ src/services/donation-service.ts
@@ -1,10 +1,12 @@
-import { inject } from 'aurelia-framework';
+import { inject, Aurelia } from 'aurelia-framework';
+import { Router } from 'aurelia-router';
+import { PLATFORM } from 'aurelia-pal';
 import { Candidate, Donation, User } from './donation-types';
 import { HttpClient } from 'aurelia-http-client';
 import { EventAggregator } from 'aurelia-event-aggregator';
 import { TotalUpdate } from './messages';
 
-@inject(HttpClient, EventAggregator)
+@inject(HttpClient, EventAggregator, Aurelia, Router)
 export class DonationService {
   users: Map<string, User> = new Map();
   candidates: Candidate[] = [];
@@ -12,7 +14,7 @@ export class DonationService {
   paymentMethods = ['Cash', 'Paypal'];
   total = 0;
 
-  constructor(private httpClient: HttpClient, private ea: EventAggregator) {
+  constructor(private httpClient: HttpClient, private ea: EventAggregator, private au: Aurelia, private router: Router) {
     httpClient.configure(http => {
       http.withBaseUrl('http://localhost:8080');
     });
@@ -44,5 +46,23 @@ export class DonationService {
     this.total = this.total + amount;
     this.ea.publish(new TotalUpdate(this.total));
     console.log('Total so far ' + this.total);
+  }
+
+  signup(firstName: string, lastName: string, email: string, password: string) {
+    this.changeRouter(PLATFORM.moduleName('app'))
+  }
+
+  async login(email: string, password: string) {
+    this.changeRouter(PLATFORM.moduleName('app'))
+  }
+
+  logout() {
+    this.changeRouter(PLATFORM.moduleName('start'))
+  }
+
+  changeRouter(module:string) {
+    this.router.navigate('/', { replace: true, trigger: false });
+    this.router.reset();
+    this.au.setRoot(PLATFORM.moduleName(module));
   }
 }
--- src/views/login.ts
+++ src/views/login.ts
@@ -1,8 +1,15 @@
+import { inject } from 'aurelia-framework';
+import { DonationService } from '../services/donation-service';
+
+@inject(DonationService)
 export class Login {
   email = 'marge@simpson.com';
   password = 'secret';
 
+  constructor(private ds: DonationService) {}
+
   login(e) {
     console.log(`Trying to log in ${this.email}`);
+    this.ds.login(this.email, this.password);
   }
 }
