3c69c9e5cd2913b781e52ebcb3c3d8ee77ba5727
Eamonn de Leastar <edeleastar@wit.ie>
2020-04-27 09:58:56 +0100

    load candidates.json from public folder

--- src/services/donation-service.ts
+++ src/services/donation-service.ts
@@ -1,7 +1,23 @@
+import { inject } from 'aurelia-framework';
 import { Candidate, Donation } from './donation-types';
+import { HttpClient } from 'aurelia-http-client';
 
+@inject(HttpClient)
 export class DonationService {
   candidates: Candidate[] = [];
   donations: Donation[] = [];
   paymentMethods = ['Cash', 'Paypal'];
+
+  constructor(private httpClient: HttpClient) {
+    httpClient.configure(http => {
+      http.withBaseUrl('http://localhost:8080');
+    });
+    this.getCandidates();
+  }
+
+  async getCandidates() {
+    const response = await this.httpClient.get('/api/candidates.json');
+    this.candidates = await response.content;
+    console.log (this.candidates);
+  }
 }
