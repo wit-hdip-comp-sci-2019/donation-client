43b9b05f99f2b04e66b9c99be008bf5ae530ef8e
Eamonn de Leastar <edeleastar@wit.ie>
2020-04-28 09:01:12 +0100

    place coordinates on donate-form

--- src/resources/elements/donate-form.ts
+++ src/resources/elements/donate-form.ts
@@ -1,22 +1,22 @@
 import { inject } from 'aurelia-framework';
 import { bindable } from 'aurelia-framework';
-import { Candidate, Donation } from '../../services/donation-types';
-import {DonationService} from "../../services/donation-service";
+import { Candidate, Location } from '../../services/donation-types';
+import { DonationService } from '../../services/donation-service';
 
 @inject(DonationService)
 export class DonateForm {
-  @bindable
-  paymentMethods: string[];
-  @bindable
-  candidates: Candidate[];
+  @bindable paymentMethods: string[];
+  @bindable candidates: Candidate[];
 
   amount = '0';
   selectedMethod = '';
-  selectedCandidate : Candidate = null;
+  selectedCandidate: Candidate = null;
 
-  constructor (private ds: DonationService) {}
+  location: Location = { lat: 53.2734, lng: -7.7783203 };
+
+  constructor(private ds: DonationService) {}
 
   makeDonation() {
-    this.ds.donate(parseInt(this.amount), this.selectedMethod, this.selectedCandidate);
+    this.ds.donate(parseInt(this.amount), this.selectedMethod, this.selectedCandidate, this.location);
   }
 }
